ca14a91a367b6e383b316cf6ae850793c18a321e
Zach Ewert <zachanima@gmail.com>
2009-10-24 01:50:18 +0200

    Add main

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "main.hpp"
+
+int main(int argc, char *argv[]) {
+  Game::initialize();
+  Game::loop();
+
+  return 0;
+}
+
---
+++ src/main.hpp
@@ -0,0 +1,7 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include "game/game.hpp"
+
+#endif // MAIN_HPP
+
