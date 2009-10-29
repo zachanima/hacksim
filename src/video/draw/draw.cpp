b22d36b91de91913bf724fe86db2030b409b0f4e
Zach Ewert <zachanima@gmail.com>
2009-10-29 18:48:47 +0100

    Add variable texture coordinates for drawrectangle

--- src/video/draw/draw.cpp
+++ src/video/draw/draw.cpp
@@ -1,11 +1,24 @@
 #include "../video.hpp"
 
 void Video::drawRectangle(SDL_Rect area) {
-  glBegin(GL_QUADS);
-    glTexCoord2s(0, 0); glVertex2i(area.x         , area.y         );
-    glTexCoord2s(0, 1); glVertex2i(area.x         , area.y + area.h);
-    glTexCoord2s(1, 1); glVertex2i(area.x + area.w, area.y + area.h);
-    glTexCoord2s(1, 0); glVertex2i(area.x + area.w, area.y         );
-  glEnd();
+  Rectf texture = { 0.0f, 0.0f, 1.0f, 1.0f };
+  drawRectangle(area, texture);
 }
 
+
+
+void Video::drawRectangle(SDL_Rect area, Rectf texture) {
+  glBegin(GL_QUADS);
+    glTexCoord2f(texture.x, texture.y);
+    glVertex2i(area.x, area.y);
+
+    glTexCoord2f(texture.x, texture.y + texture.w);
+    glVertex2i(area.x, area.y + area.h);
+
+    glTexCoord2f(texture.x + texture.w, texture.y + texture.w);
+    glVertex2i(area.x + area.w, area.y + area.h);
+
+    glTexCoord2f(texture.x + texture.w, texture.y);
+    glVertex2i(area.x + area.w, area.y);
+  glEnd();
+}
--- src/video/video.hpp
+++ src/video/video.hpp
@@ -5,12 +5,20 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_opengl.h>
 
+typedef struct {
+  float x;
+  float y;
+  float w;
+  float h;
+} Rectf;
+
 class Video {
   public:
     static void initialize(std::string);
     static void update();
 
-    static void drawRectangle(SDL_Rect area);
+    static void drawRectangle(SDL_Rect);
+    static void drawRectangle(SDL_Rect, Rectf);
 
   private:
     static unsigned int width;
