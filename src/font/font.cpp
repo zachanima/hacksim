a76f3064b1fda7329b2b86b831bfcdd7744f6063
Zach Ewert <zachanima@gmail.com>
2009-10-29 08:35:02 +0100

    Move font delta factors to where they're updated

--- src/font/font.cpp
+++ src/font/font.cpp
@@ -7,14 +7,14 @@ void Font::draw(std::string string, SDL_Rect area) {
   glColor3f(0.75f, 0.75f, 0.75f);
   glBegin(GL_QUADS);
   BOOST_FOREACH(char c, string) {
-    const SDL_Rect rectangle = { area.x + dx * 6, area.y + dy * 8, 5, 7 };
+    const SDL_Rect rectangle = { area.x + dx, area.y + dy, 5, 7 };
     Video::drawRectangle(rectangle);
 
     if (rectangle.x >= area.w || c == '\n') {
       dx = 0;
-      dy++;
+      dy += 7 + 1;
     } else {
-      dx++;
+      dx += 5 + 1;
     }
   }
   glEnd();
