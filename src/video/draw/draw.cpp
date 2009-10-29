5d35d8bd9c39fd607dd36ee4924ffaaf29a51a8a
Zach Ewert <zachanima@gmail.com>
2009-10-29 08:42:00 +0100

    Add gltexcoord2s calls to drawrectangle function

--- src/video/draw/draw.cpp
+++ src/video/draw/draw.cpp
@@ -2,10 +2,10 @@
 
 void Video::drawRectangle(SDL_Rect area) {
   glBegin(GL_QUADS);
-    glVertex2i(area.x         , area.y         );
-    glVertex2i(area.x         , area.y + area.h);
-    glVertex2i(area.x + area.w, area.y + area.h);
-    glVertex2i(area.x + area.w, area.y         );
+    glTexCoord2s(0, 0); glVertex2i(area.x         , area.y         );
+    glTexCoord2s(0, 1); glVertex2i(area.x         , area.y + area.h);
+    glTexCoord2s(1, 1); glVertex2i(area.x + area.w, area.y + area.h);
+    glTexCoord2s(1, 0); glVertex2i(area.x + area.w, area.y         );
   glEnd();
 }
 
