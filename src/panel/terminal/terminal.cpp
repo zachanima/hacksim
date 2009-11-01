8523c16bb9b9b5d3a27923d494533f1fdb06d156
Zach Ewert <zachanima@gmail.com>
2009-11-01 19:43:27 +0100

    Add backspace functionality to terminalpanel key

--- src/panel/terminal/terminal.cpp
+++ src/panel/terminal/terminal.cpp
@@ -1,7 +1,24 @@
 #include "terminal.hpp"
 
 void TerminalPanel::key(SDL_keysym key) {
-  this->command += key.unicode;
+  switch (key.sym) {
+    case SDLK_BACKSPACE:
+      if (!this->command.empty()) {
+        this->command.erase(this->command.end() - 1);
+      }
+      break;
+
+    case SDLK_h: // CTRL + H == backspace.
+      if ((key.mod & KMOD_CTRL) && !this->command.empty()) {
+        this->command.erase(this->command.end() - 1);
+        break;
+      }
+      // Fallthrough.
+
+    default:
+      this->command += key.unicode;
+      break;
+  }
 }
 
 
@@ -14,6 +31,6 @@ void TerminalPanel::draw() {
   SDL_Rect padded = { PADDING, PADDING, this->area.w - (PADDING + PADDING), this->area.h - (PADDING + PADDING) };
 
   glColor3f(1.0f, 1.0f, 1.0f);
-  this->font->draw(this->buffer + this->command, padded);
+  this->font->draw(this->buffer + this->command + "_", padded);
 }
 
